fd298deb57b544e74fc0ae450147a2d9834a477e
an-seunghwan <dpeltms79@gmail.com>
2022-07-29 21:46:31 +0900

    add finding true DAG main code

--- utils/pendulum.py
+++ utils/pendulum.py
@@ -9,30 +9,32 @@
 #%%
 import matplotlib.pyplot as plt
 import os
-
-import matplotlib.image as mpimg
-import random
 import math
 import numpy as np
 import pandas as pd 
-
+import tqdm
+import warnings
+warnings.filterwarnings('ignore')
+#%%
 if not os.path.exists('./causal_data/pendulum/'): 
-  os.makedirs('./causal_data/pendulum/train/')
-  os.makedirs('./causal_data/pendulum/test/')
+    os.makedirs('./causal_data/pendulum/train/')
+    os.makedirs('./causal_data/pendulum/test/')
 
 def projection(theta, phi, x, y, base = -0.5):
-    b = y-x*math.tan(phi)
-    shade = (base - b)/math.tan(phi)
+    b = y - x * math.tan(phi)
+    shade = (base - b) / math.tan(phi)
     return shade
 #%%
 scale = np.array([[0,44],[100,40],[7,7.5],[10,10]])
 count = 0
-empty = pd.DataFrame(columns=['i', 'j', 'shade','mid'])
-for i in range(-40, 44): # pendulum
+train = pd.DataFrame(columns=['i', 'j', 'shade','mid'])
+test = pd.DataFrame(columns=['i', 'j', 'shade','mid'])
+for i in tqdm.tqdm(range(-40, 44)): # pendulum
     for j in range(60, 148): # light
         if j == 100:
             continue
         plt.rcParams['figure.figsize'] = (1.0, 1.0)
+        
         theta = i*math.pi/200.0
         phi = j*math.pi/200.0
         x = 10 + 8*math.sin(theta)
@@ -43,7 +45,6 @@ for i in range(-40, 44): # pendulum
 
         light = projection(theta, phi, 10, 10.5, 20.5)
         sun = plt.Circle((light,20.5), 3, color = 'orange')
-
 
         #calculate the mid index of 
         ball_x = 10+9.5*math.sin(theta)
@@ -61,20 +62,19 @@ for i in range(-40, 44): # pendulum
         ax.set_xlim((0, 20))
         ax.set_ylim((-1, 21))
         new=pd.DataFrame({
-                  'i':(i-scale[0][0])/(scale[0][1]-0),
-                  'j':(j-scale[1][0])/(scale[1][1]-0),
-                  'shade':(shade-scale[2][0])/(scale[2][1]-0),
-                  'mid':(mid-scale[2][0])/(scale[2][1]-0)
-                  },
-                  
-                 index=[1])
-        empty=empty.append(new,ignore_index=True)
+                        'i':(i-scale[0][0])/(scale[0][1]-0),
+                        'j':(j-scale[1][0])/(scale[1][1]-0),
+                        'shade':(shade-scale[2][0])/(scale[2][1]-0),
+                        'mid':(mid-scale[2][0])/(scale[2][1]-0)
+                        }, index=[1])
         plt.axis('off')
         if count == 4:
-          plt.savefig('./causal_data/pendulum/test/a_' + str(int(i)) + '_' + str(int(j)) + '_' + str(int(shade)) + '_' + str(int(mid)) +'.png',dpi=96)
-          count = 0
+            plt.savefig('./causal_data/pendulum/test/a_' + str(int(i)) + '_' + str(int(j)) + '_' + str(int(shade)) + '_' + str(int(mid)) +'.png',dpi=96)
+            count = 0
+            train = train.append(new, ignore_index=True)
         else:
-          plt.savefig('./causal_data/pendulum/train/a_' + str(int(i)) + '_' + str(int(j)) + '_' + str(int(shade)) + '_' + str(int(mid)) +'.png',dpi=96)
+            plt.savefig('./causal_data/pendulum/train/a_' + str(int(i)) + '_' + str(int(j)) + '_' + str(int(shade)) + '_' + str(int(mid)) +'.png',dpi=96)
+            test = test.append(new, ignore_index=True)
         plt.clf()
         count += 1
 #%%
