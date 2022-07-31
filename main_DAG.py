b62c233829847463ff953906c6d5ae13f75f4255
an-seunghwan <dpeltms79@gmail.com>
2022-07-31 16:19:39 +0900

    update true DAG finding
    Causal VAE official code and extract only causal structure learning part, (objective) = DAG recon + 3*h + 0.5*h^2, but DAG cannot be found

--- main_DAG.py
+++ main_DAG.py
@@ -13,6 +13,8 @@ plt.switch_backend('agg')
 import torch
 from torch.utils.data import TensorDataset, DataLoader
 
+from utils.model_DAG import MaskLayer
+
 from utils.simulation import (
     set_random_seed,
     is_dag,
@@ -49,39 +51,39 @@ import argparse
 def get_args(debug):
     parser = argparse.ArgumentParser('parameters')
 
-    parser.add_argument('--seed', type=int, default=18, 
+    parser.add_argument('--seed', type=int, default=1, 
                         help='seed for repeatable results')
 
     parser.add_argument('--d', default=4, type=int,
                         help='the number of nodes')
-    parser.add_argument('--batch_size', default=128, type=int,
+    parser.add_argument('--batch_size', default=64, type=int,
                         help='batch size')
     
     parser.add_argument('--rho', default=1, type=float,
                         help='rho')
-    parser.add_argument('--alpha', default=0, type=float,
+    parser.add_argument('--alpha', default=3, type=float,
                         help='alpha')
-    parser.add_argument('--h', default=np.inf, type=float,
-                        help='h')
+    # parser.add_argument('--h', default=np.inf, type=float,
+    #                     help='h')
     
     parser.add_argument('--lr', default=0.001, type=float,
                         help='learning rate')
-    parser.add_argument('--max_iter', default=100, type=int,
-                        help='maximum iteration')
+    # parser.add_argument('--max_iter', default=100, type=int,
+    #                     help='maximum iteration')
     parser.add_argument('--epochs', default=100, type=int,
                         help='maximum iteration')
-    parser.add_argument('--h_tol', default=1e-8, type=float,
-                        help='h value tolerance')
-    parser.add_argument('--w_threshold', default=0.3, type=float,
-                        help='weight adjacency matrix threshold')
-    parser.add_argument('--lambda', default=0.1, type=float,
-                        help='weight of LASSO regularization')
-    parser.add_argument('--progress_rate', default=0.25, type=float,
-                        help='progress rate')
-    parser.add_argument('--rho_max', default=1e+16, type=float,
-                        help='rho max')
-    parser.add_argument('--rho_rate', default=2, type=float,
-                        help='rho rate')
+    # parser.add_argument('--h_tol', default=1e-8, type=float,
+    #                     help='h value tolerance')
+    # parser.add_argument('--w_threshold', default=0.3, type=float,
+    #                     help='weight adjacency matrix threshold')
+    # parser.add_argument('--lambda', default=0.1, type=float,
+    #                     help='weight of LASSO regularization')
+    # parser.add_argument('--progress_rate', default=0.25, type=float,
+    #                     help='progress rate')
+    # parser.add_argument('--rho_max', default=1e+16, type=float,
+    #                     help='rho max')
+    # parser.add_argument('--rho_rate', default=2, type=float,
+    #                     help='rho rate')
 
     if debug:
         return parser.parse_args(args=[])
@@ -93,11 +95,11 @@ def h_fun(W):
     h = trace_expm(W * W) - W.shape[0]
     return h
 
-def train(dataloader, B_est, alpha, rho, config, optimizer):
+def train(dataloader, model, B_est, config, optimizer):
     logs = {
         'loss': [], 
         'recon': [],
-        'L1': [],
+        # 'L1': [],
         'aug': [],
     }
     
@@ -109,16 +111,16 @@ def train(dataloader, B_est, alpha, rho, config, optimizer):
         """Evaluate value and gradient of augmented Lagrangian."""
         loss_ = []
         
-        R = batch - batch.matmul(B_est)
-        recon = 0.5 * (R ** 2).sum(axis=1).mean()
+        mse = torch.nn.MSELoss()
+        recon = mse(model(torch.matmul(batch, B_est)), batch)
         loss_.append(('recon', recon))
         
-        L1 = config["lambda"] * torch.norm(B_est, p=1)
-        loss_.append(('L1', L1))
+        # L1 = config["lambda"] * torch.norm(B_est, p=1)
+        # loss_.append(('L1', L1))
         
         h = h_fun(B_est)
-        aug = 0.5 * rho * (h ** 2)
-        aug += alpha * h
+        aug = config["alpha"] * h
+        aug += 0.5 * config["rho"] * (h ** 2)
         loss_.append(('aug', aug))
             
         loss = sum([y for _, y in loss_])
@@ -146,8 +148,8 @@ def main():
 
     train_imgs = os.listdir('./utils/causal_data/pendulum/train')
     label = np.array([x[:-4].split('_')[1:] for x in train_imgs]).astype(float)
-    label = label - label.mean(axis=0) # logit
-    label = label / label.std(axis=0)
+    # label = label - label.mean(axis=0) # logit
+    # label = label / label.std(axis=0)
     
     label = torch.Tensor(label) 
     dataset = TensorDataset(label) 
@@ -155,52 +157,30 @@ def main():
                             batch_size=config["batch_size"],
                             shuffle=True)
     
+    model = MaskLayer() # nonlinear transformation
     B_est = torch.zeros((config["d"], config["d"]), 
                         requires_grad=True)
 
-    # initial values
-    rho = config["rho"]
-    alpha = config["alpha"]
-    h = config["h"]
-
-    optimizer = torch.optim.Adam([B_est], lr=config["lr"])
+    optimizer = torch.optim.Adam(list(model.parameters()) + [B_est], lr=config["lr"])
+    model.train()
     
-    for iteration in range(config["max_iter"]):
-        """primal update"""
-        h_old = np.inf
-        while rho < config["rho_max"]:
-            for epoch in tqdm.tqdm(range(config["epochs"]), desc="primal update"):
-            # while True:
-                logs, B_est = train(dataloader, B_est, alpha, rho, config, optimizer)
-                
-                # """stopping rule: no change in weight estimation (convergence)"""
-                # with torch.no_grad():
-                #     h_new = h_fun(B_est).item()
-                # if abs(h_old - h_new) < 1e-8: 
-                #     break
-                # h_old = h_new
+    # for epoch in tqdm.tqdm(range(config["epochs"]), desc="Finding True DAG..."):
+    for epoch in range(config["epochs"]):
+        logs, B_est = train(dataloader, model, B_est, config, optimizer)
         
-            with torch.no_grad():
-                h_new = h_fun(B_est).item()
-            if h_new > config["progress_rate"] * h:
-                rho *= config["rho_rate"]
-            else:
-                break
-        
-        """dual ascent step"""
-        h = h_new
-        alpha += rho * h
-        
-        print_input = "[iteration {:03d}]".format(iteration + 1)
+        print_input = "[EPOCH {:03d}]".format(epoch + 1)
         print_input += ''.join([', {}: {:.4f}'.format(x, np.mean(y).round(2)) for x, y in logs.items()])
         print(print_input)
         
         """update log"""
-        wandb.log({x : np.mean(y) for x, y in logs.items()})
+        if epoch % 10 == 0:
+            wandb.log({x : np.mean(y) for x, y in logs.items()})
         
-        """stopping rule"""
-        if h <= config["h_tol"] or rho >= config["rho_max"]:
-            break
+        # """stopping rule"""
+        # if h <= config["h_tol"] or rho >= config["rho_max"]:
+        #     break
+    
+    B_est
     
     wandb.run.finish()
 #%%
