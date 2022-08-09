cdc05d05c5fc179d163c8d2493750a85ae3d61ab
an-seunghwan <dpeltms79@gmail.com>
2022-08-09 14:20:03 +0900

    Update pendulum.py

--- utils/pendulum.py
+++ utils/pendulum.py
@@ -26,41 +26,50 @@ count = 0
 train = pd.DataFrame(columns=['light', 'angle', 'length', 'position'])
 test = pd.DataFrame(columns=['light', 'angle', 'length', 'position'])
 #%%
+"""Addictive Noise Data Generating Process"""
+np.random.seed(1)
 varphi = -0.1 # -1 ~ 1
 theta = 0.1 # -1 ~ 1
 center = (10, 10.5)
 threshold = 45
 l = 8
+scale = 0.1
 for varphi in tqdm.tqdm(np.linspace(-1, 1, 100)):
     for theta in np.linspace(-1, 1, 100):
         objects = []
         
-        light = center[0] - center[1] * math.tan(math.radians(varphi * threshold))
-        ball = (center[0] + l * math.sin(math.radians(theta * threshold)),
-                center[1] - l * math.cos(math.radians(theta * threshold)))
+        varphi_ = varphi
+        theta_ = theta
+        # varphi_ = varphi + np.random.normal(scale=0.1) 
+        # theta_ = theta + np.random.normal(scale=0.1)
+        
+        light = center[0] - center[1] * math.tan(math.radians(varphi_ * threshold))
+        
+        ball = (center[0] + l * math.sin(math.radians(theta_ * threshold)),
+                center[1] - l * math.cos(math.radians(theta_ * threshold)))
+        ball_x = center[0] + (l + 1.5) * math.sin(math.radians(theta_ * threshold))
+        ball_y = center[0] - (l + 1.5) * math.cos(math.radians(theta_ * threshold))
 
+        tan_phi = light - ball[0]
+        tan_phi /= 20.5 - ball[1]
+        shadow_start = center[0] + center[1] * math.tan(math.radians(varphi_ * threshold))
+
+        length = light - 20.5 * tan_phi - shadow_start + np.random.normal(scale=scale)
+        position = shadow_start + length / 2 + np.random.normal(scale=scale)
+        
+        objects.append(('light', varphi_))
+        objects.append(('theta', theta_))
+        objects.append(('length', length))
+        objects.append(('position', position))
+        # name = '_'.join([str(int(y)) for x,y in objects])
+        name = '_'.join([str(round(y, 2)) for x,y in objects])
+        
         plt.rcParams['figure.figsize'] = (1.0, 1.0)
                 
         gun = plt.Polygon(([10, 10.5], ball), color = 'black', linewidth = 3)
         pendulum = plt.Circle(ball, 1.5, color = 'firebrick')
         sun = plt.Circle((light, 20.5), 3, color = 'orange')
-
-        ball_x = center[0] + (l + 1.5) * math.sin(math.radians(theta * threshold))
-        ball_y = center[0] - (l + 1.5) * math.cos(math.radians(theta * threshold))
-
-        tan_phi = light - ball[0]
-        tan_phi /= 20.5 - ball[1]
-        shadow_start = center[0] + center[1] * math.tan(math.radians(varphi * threshold))
-        length = light - 20.5 * tan_phi - shadow_start
-
         shadow = plt.Polygon(([shadow_start, -0.5], [shadow_start + length, -0.5]), color = 'black', linewidth = 4)
-        position = shadow_start + length / 2
-        
-        objects.append(('light', light))
-        objects.append(('theta', theta))
-        objects.append(('length', length))
-        objects.append(('position', position))
-        name = '_'.join([str(int(y)) for x,y in objects])
         
         ax = plt.gca()
         ax.add_artist(sun)
@@ -83,4 +92,10 @@ for varphi in tqdm.tqdm(np.linspace(-1, 1, 100)):
         # plt.show()
         plt.clf()
         count += 1
+#%%
+# train_imgs = os.listdir('./causal_data/pendulum/train')
+# len(train_imgs)
+# label = np.array([x[:-4].split('_')[1:] for x in train_imgs]).astype(float)
+# label.std(axis=0)
+# label.mean(axis=0).round(2)
 #%%
