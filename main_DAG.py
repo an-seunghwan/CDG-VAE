d5e767a57e29c00506e4c3bd5549a41b3a70c46c
an-seunghwan <dpeltms79@gmail.com>
2022-08-09 20:40:20 +0900

    default cpu option

--- main_DAG.py
+++ main_DAG.py
@@ -153,8 +153,10 @@ def train(dataloader, model, B_est, mask, rho, alpha, config, optimizer):
 #%%
 def main():
     config = vars(get_args(debug=False)) # default configuration
-    config["cuda"] = torch.cuda.is_available()
-    device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    # config["cuda"] = torch.cuda.is_available()
+    # device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+    config["cuda"] = False
+    device = torch.device('cpu')
     wandb.config.update(config)
 
     set_random_seed(config["seed"])
