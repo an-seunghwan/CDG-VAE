02698fefa25dcda0c779c8566c1710f7f7f492e3
an-seunghwan <dpeltms79@gmail.com>
2022-09-07 09:56:27 +0900

    Update sweep.py

--- sweep.py
+++ sweep.py
@@ -10,7 +10,7 @@ try:
     import wandb
 except:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "wandb"])
-    with open("../wandb_api.txt", "r") as f:
+    with open("./wandb_api.txt", "r") as f:
         key = f.readlines()
     subprocess.run(["wandb", "login"], input=key[0], encoding='utf-8')
     import wandb
