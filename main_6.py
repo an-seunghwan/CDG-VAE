309f80955dfef2213e14fee5babbd6f57ca13347
an-seunghwan <dpeltms79@gmail.com>
2022-09-08 18:26:22 +0900

    Update main_6.py

--- main_6.py
+++ main_6.py
@@ -169,7 +169,7 @@ def train(dataloader, model, config, optimizer, device):
     return logs, xhat
 #%%
 def main():
-    config = vars(get_args(debug=True)) # default configuration
+    config = vars(get_args(debug=False)) # default configuration
     config["cuda"] = torch.cuda.is_available()
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
     wandb.config.update(config)
