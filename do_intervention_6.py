81f2cf9cde7963723436ccdb8912349eaea0d62d
an-seunghwan <dpeltms79@gmail.com>
2022-09-13 20:33:00 +0900

    Update do_intervention_6.py

--- do_intervention_6.py
+++ do_intervention_6.py
@@ -53,7 +53,7 @@ def get_args(debug):
     
     parser.add_argument('--version', type=int, default=6, 
                         help='model version')
-    parser.add_argument('--num', type=int, default=7, 
+    parser.add_argument('--num', type=int, default=0, 
                         help='model version')
 
     if debug:
@@ -62,7 +62,7 @@ def get_args(debug):
         return parser.parse_args()
 #%%
 def main():
-    config = vars(get_args(debug=True)) # default configuration
+    config = vars(get_args(debug=False)) # default configuration
     
     """model load"""
     artifact = wandb.use_artifact('anseunghwan/(causal)VAE/model_{}:v{}'.format(config["version"], config["num"]), type='model')
