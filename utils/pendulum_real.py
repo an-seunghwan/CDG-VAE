8d76864a03b9162b87de0f0a573f5a00d2566430
an-seunghwan <dpeltms79@gmail.com>
2022-10-04 19:40:12 +0900

    Update pendulum_real.py

--- utils/pendulum_real.py
+++ utils/pendulum_real.py
@@ -17,9 +17,18 @@ import tqdm
 import warnings
 warnings.filterwarnings('ignore')
 #%%
-if not os.path.exists('./causal_data/pendulum/'): 
-    os.makedirs('./causal_data/pendulum/train')
-    os.makedirs('./causal_data/pendulum/test')
+"""
+For main experiments and sample efficiency
+Add followings to ./utils/pendulum.py
+[1]: measurement error
+[2]: environmental disturbance
+[3]: target label
+"""
+#%%
+foldername = 'pendulum_real'
+if not os.path.exists('./causal_data/{}/'.format(foldername)): 
+    os.makedirs('./causal_data/{}/train'.format(foldername))
+    os.makedirs('./causal_data/{}/test'.format(foldername))
 #%%
 train = pd.DataFrame(columns=['light', 'angle', 'length', 'position'])
 test = pd.DataFrame(columns=['light', 'angle', 'length', 'position'])
@@ -27,78 +36,91 @@ test = pd.DataFrame(columns=['light', 'angle', 'length', 'position'])
 """Data Generating Process"""
 np.random.seed(1)
 
-# light_angle_list= np.random.uniform(math.pi/4, math.pi/2, 100)
-# pendulum_angle_list = np.random.uniform(0, math.pi/4, 100)
-light_angle_list= np.linspace(math.pi/4, math.pi/2, 100)
-pendulum_angle_list = np.linspace(0, math.pi/4, 100)
+light_angle_list= np.random.uniform(math.pi/4, math.pi/2, 100)
+pendulum_angle_list = np.random.uniform(0, math.pi/4, 100)
+# light_angle_list= np.linspace(math.pi/4, math.pi/2, 10)
+# pendulum_angle_list = np.linspace(0, math.pi/4, 10)
+
 center = (10, 10.5) # (c_x, c_y) : the axis's of the center     
 l = 9.5  # length of pendulum (including the red ball)
 b = -0.5
 #%%
 count = 0
-for pendulum_angle in tqdm.tqdm(pendulum_angle_list):
-    for light_angle in light_angle_list:
-        objects = []
-        
-        xi_1 = pendulum_angle
-        xi_2 = light_angle
-        
-        light = center[0] + (10 / math.tan(xi_2))
-        
-        x = 10 + (l - 1.5) * math.sin(xi_1)
-        y = 10 - (l - 1.5) * math.cos(xi_1)
-        
-        # xi_3 : shadow_length
-        # xi_4 : shadow_position
-        xi_3 = (center[0] + l * math.sin(xi_1) - (center[1] - l * math.cos(xi_1) - b) / math.tan(xi_2) ) 
-        xi_3 -= (center[0] - (center[1] - b) / math.tan(xi_2))
-        xi_4 = center[0] + l * math.sin(xi_1) - (center[1] - l * math.cos(xi_1) - b) / math.tan(xi_2) 
-        xi_4 += (center[0] - (center[1] - b) / math.tan(xi_2))
-        xi_4 /= 2
-        
-        objects.append(('light', xi_2))
-        objects.append(('angle', xi_1))
-        objects.append(('length', xi_3))
-        objects.append(('position', xi_4))
-        name = '_'.join([str(round(j, 4)) for i,j in objects])
-        
-        plt.rcParams['figure.figsize'] = (1.0, 1.0)
-        
-        sun = plt.Circle((light, 20.5), 3, color = 'orange') 
-        gun = plt.Polygon(([10, 10.5],[x,y]), color = 'black', linewidth = 3)
-        ball = plt.Circle((x,y), 1.5, color = 'firebrick')
-        shadow = plt.Polygon(([xi_4 - xi_3 / 2.0, -0.5],[xi_4 + xi_3 / 2.0, -0.5]), color = 'black', linewidth = 3)
-        
-        ax = plt.gca()
-        ax.add_artist(sun)
-        ax.add_artist(gun)
-        ax.add_artist(ball)
-        ax.add_artist(shadow)
-       
-        ax.set_xlim((0, 20))
-        ax.set_ylim((-2, 22))
-        plt.axis('off')
-        
-        new = pd.DataFrame({i:j for i,j in objects}, index=[1])
-        plt.axis('off')
-        if count == 4:
-            plt.savefig('./causal_data/pendulum/test/a_' + name +'.png',dpi=96)
-            test = test.append(new, ignore_index=True)
-            count = 0
-        else:
-            plt.savefig('./causal_data/pendulum/train/a_' + name +'.png',dpi=96)
-            train = train.append(new, ignore_index=True)
-        # plt.show()
-        plt.clf()
-        count += 1
+scale = 0.1 # measurement error scale
+for light_angle, pendulum_angle in tqdm.tqdm(zip(light_angle_list, pendulum_angle_list)):
+    objects = []
+    
+    xi_1 = light_angle
+    xi_2 = pendulum_angle
+    
+    light = center[0] + (10 / math.tan(xi_1))
+    
+    x = 10 + (l - 1.5) * math.sin(xi_2)
+    y = 10 - (l - 1.5) * math.cos(xi_2)
+    
+    # xi_3 : shadow_length
+    # xi_4 : shadow_position
+    xi_3 = (center[0] + l * math.sin(xi_2) - (center[1] - l * math.cos(xi_2) - b) / math.tan(xi_1) ) 
+    xi_3 -= (center[0] - (center[1] - b) / math.tan(xi_1))
+    xi_4 = center[0] + l * math.sin(xi_2) - (center[1] - l * math.cos(xi_2) - b) / math.tan(xi_1) 
+    xi_4 += (center[0] - (center[1] - b) / math.tan(xi_1))
+    xi_4 /= 2
+    
+    """measurement error"""
+    xi_3 += np.random.normal(loc=0, scale=scale)
+    xi_4 += np.random.normal(loc=0, scale=scale)
+    
+    tau = 0
+    """data corruption: 20%"""
+    if (count + 1) % 5 == 0:
+        xi_3 = np.random.uniform(low=0, high=12)
+        xi_3 = np.random.uniform(low=0, high=12)
+        tau = 1
+    
+    objects.append(('light', xi_1))
+    objects.append(('angle', xi_2))
+    objects.append(('length', xi_3))
+    objects.append(('position', xi_4))
+    objects.append(('target', tau))
+    name = '_'.join([str(round(j, 4)) for i,j in objects])
+    
+    plt.rcParams['figure.figsize'] = (1.0, 1.0)
+    
+    sun = plt.Circle((light, 20.5), 3, color = 'orange') 
+    gun = plt.Polygon(([10, 10.5], [x,y]), color = 'black', linewidth = 3)
+    ball = plt.Circle((x, y), 1.5, color = 'firebrick')
+    shadow = plt.Polygon(([xi_4 - xi_3 / 2.0, -0.5], [xi_4 + xi_3 / 2.0, -0.5]), color = 'black', linewidth = 3)
+    
+    ax = plt.gca()
+    ax.add_artist(sun)
+    ax.add_artist(gun)
+    ax.add_artist(ball)
+    ax.add_artist(shadow)
+    
+    ax.set_xlim((0, 20))
+    ax.set_ylim((-2, 22))
+    plt.axis('off')
+    
+    new = pd.DataFrame({i:j for i,j in objects}, index=[1])
+    plt.axis('off')
+    if (count + 1) % 4 == 0:
+        plt.savefig('./causal_data/{}/test/a_' .format(foldername)+ name +'.png',dpi=96)
+        test = test.append(new, ignore_index=True)
+    else:
+        plt.savefig('./causal_data/{}/train/a_'.format(foldername) + name +'.png',dpi=96)
+        train = train.append(new, ignore_index=True)
+    # plt.show()
+    plt.clf()
+    
+    count += 1
 #%%
-# train_imgs = [x for x in os.listdir('./causal_data/pendulum/train') if x.endswith('.png')]
-# len(train_imgs)
-# test_imgs = [x for x in os.listdir('./causal_data/pendulum/test') if x.endswith('.png')]
-# len(test_imgs)
-# label = np.array([x[:-4].split('_')[1:] for x in train_imgs]).astype(float)
-# label.std(axis=0).round(2)
-# label.mean(axis=0).round(2)
+train_imgs = [x for x in os.listdir('./causal_data/{}/train'.format(foldername)) if x.endswith('.png')]
+len(train_imgs)
+test_imgs = [x for x in os.listdir('./causal_data/{}/test'.format(foldername)) if x.endswith('.png')]
+len(test_imgs)
+label = np.array([x[:-4].split('_')[1:] for x in train_imgs]).astype(float)
+label.std(axis=0).round(2)
+label.mean(axis=0).round(2)
 #%%
 # train_imgs = [x for x in os.listdir('./causal_data/pendulum/train') if x.endswith('.png')]
 # img = np.array(Image.open('./causal_data/pendulum/train/' + train_imgs[0]).resize((64, 64)))[:, :, :3].astype(float)
