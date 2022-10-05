eef4881fbd9db47ab092a7b9473b4a6b3ca1a4d3
an-seunghwan <dpeltms79@gmail.com>
2022-10-05 22:23:00 +0900

    add test dataset

--- modules/datasets.py
+++ modules/datasets.py
@@ -68,3 +68,36 @@ class UnLabeledDataset(Dataset):
         x = torch.FloatTensor(self.x_data[idx])
         return x
 #%%
+class TestDataset(Dataset): 
+    def __init__(self, config):
+        if config["DR"]:
+            foldername = 'pendulum_DR'
+            self.name = ['light', 'angle', 'length', 'position', 'background', 'target']
+        else:
+            foldername = 'pendulum_real'
+            self.name = ['light', 'angle', 'length', 'position', 'target']
+        test_imgs = [x for x in os.listdir('./modules/causal_data/{}/test'.format(foldername)) if x.endswith('png')]
+        
+        test_x = []
+        for i in tqdm.tqdm(range(len(test_imgs)), desc="test data loading"):
+            test_x.append(np.array(
+                Image.open("./modules/causal_data/{}/test/{}".format(foldername, test_imgs[i])).resize(
+                    (config["image_size"], config["image_size"])))[:, :, :3])
+        self.x_data = (np.array(test_x).astype(float) - 127.5) / 127.5
+        
+        label = np.array([x[:-4].split('_')[1:] for x in test_imgs]).astype(float)
+        label = label - label.mean(axis=0)
+        self.std = label.std(axis=0)
+        """bounded label: normalize to (0, 1)"""
+        if config["label_normalization"]: 
+            label = (label - label.min(axis=0)) / (label.max(axis=0) - label.min(axis=0))
+        self.y_data = label
+
+    def __len__(self): 
+        return len(self.x_data)
+
+    def __getitem__(self, idx): 
+        x = torch.FloatTensor(self.x_data[idx])
+        y = torch.FloatTensor(self.y_data[idx])
+        return x, y
+#%%
