4a50ab16893d5c3abf4edd6907ecdbce73e7c0b1
an-seunghwan <dpeltms79@gmail.com>
2022-11-24 14:21:14 +0900

    Create datasets.py

---
+++ tabular/modules/datasets.py
@@ -0,0 +1,101 @@
+#%%
+import tqdm
+import os
+import numpy as np
+import pandas as pd
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import Dataset
+#%%
+class TabularDataset(Dataset): 
+    def __init__(self, config):
+        """
+        load dataset: Personal Loan
+        Reference: https://www.kaggle.com/datasets/teertha/personal-loan-modeling
+        """
+        df = pd.read_csv('./data/Bank_Personal_Loan_Modelling.csv')
+        df = df.sample(frac=1, random_state=config["seed"]).reset_index(drop=True)
+        df = df.drop(columns=['ID'])
+            
+        self.continuous = ['CCAvg', 'Mortgage', 'Income', 'Experience', 'Age']
+        self.topology = [['CCAvg', 'Mortgage', 'Income'], ['Experience'], ['Age']]
+        df = df[self.continuous]
+        
+        from sklearn.decomposition import PCA
+        pca1 = PCA(n_components=2).fit_transform(df[self.topology[0]])
+        pca2 = PCA(n_components=1).fit_transform(df[self.topology[1]])
+        pca3 = PCA(n_components=1).fit_transform(df[self.topology[2]])
+        label = np.concatenate([pca1, pca2, pca3], axis=1)
+        
+        """bounded label: normalize to (0, 1)"""
+        if config["label_normalization"]: 
+            # label = (label - label.min(axis=0)) / (label.max(axis=0) - label.min(axis=0)) # global statistic
+            label = (label - label.mean(axis=0)) / label.std(axis=0)
+        self.label = label[:4000, :]
+        
+        train = df.iloc[:4000]
+        
+        # continuous
+        mean = train.mean(axis=0)
+        std = train.std(axis=0)
+        train = (train - mean) / std # local statistic
+        
+        self.train = train
+        self.x_data = train.to_numpy()
+        
+    def __len__(self): 
+        return len(self.x_data)
+
+    def __getitem__(self, idx): 
+        x = torch.FloatTensor(self.x_data[idx])
+        y = torch.FloatTensor(self.label[idx])
+        return x, y
+#%%
+class TestTabularDataset(Dataset): 
+    def __init__(self, config):
+        """
+        load dataset: Personal Loan
+        Reference: https://www.kaggle.com/datasets/teertha/personal-loan-modeling
+        """
+        df = pd.read_csv('./data/Bank_Personal_Loan_Modelling.csv')
+        df = df.sample(frac=1, random_state=config["seed"]).reset_index(drop=True)
+        df = df.drop(columns=['ID'])
+            
+        self.continuous = ['CCAvg', 'Mortgage', 'Income', 'Experience', 'Age']
+        self.topology = [['CCAvg', 'Mortgage', 'Income'], ['Experience'], ['Age']]
+        df = df[self.continuous]
+        
+        from sklearn.decomposition import PCA
+        pca1 = PCA(n_components=2).fit_transform(df[self.topology[0]])
+        pca2 = PCA(n_components=1).fit_transform(df[self.topology[1]])
+        pca3 = PCA(n_components=1).fit_transform(df[self.topology[2]])
+        label = np.concatenate([pca1, pca2, pca3], axis=1)
+        
+        """bounded label: normalize to (0, 1)"""
+        if config["label_normalization"]: 
+            # label = (label - label.min(axis=0)) / (label.max(axis=0) - label.min(axis=0)) # global statistic
+            label = (label - label.mean(axis=0)) / label.std(axis=0)
+        self.label = label[4000:, :]
+        
+        train = df.iloc[:4000]
+        test = df.iloc[4000:]
+        
+        # continuous
+        mean = train.mean(axis=0)
+        std = train.std(axis=0)
+        test = (test - mean) / std # local statistic
+        
+        self.test = test
+        self.x_data = test.to_numpy()
+        
+    def __len__(self): 
+        return len(self.x_data)
+
+    def __getitem__(self, idx): 
+        x = torch.FloatTensor(self.x_data[idx])
+        y = torch.FloatTensor(self.label[idx])
+        return x, y
+#%%
