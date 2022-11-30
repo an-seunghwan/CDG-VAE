9e76a31cb2b0a01e766b6e60381aa075a1d527de
an-seunghwan <dpeltms79@gmail.com>
2022-11-30 11:05:03 +0900

    tabular: add adult dataset

--- tabular/modules/datasets.py
+++ tabular/modules/datasets.py
@@ -31,41 +31,88 @@ def interleave_float(a: float, b: float):
 #%%
 class TabularDataset(Dataset): 
     def __init__(self, config):
-        """
-        load dataset: Personal Loan
-        Reference: https://www.kaggle.com/datasets/teertha/personal-loan-modeling
-        """
-        df = pd.read_csv('./data/Bank_Personal_Loan_Modelling.csv')
-        df = df.sample(frac=1, random_state=1).reset_index(drop=True)
-        df = df.drop(columns=['ID'])
+        if config["dataset"] == 'loan':
+            """
+            load dataset: Personal Loan
+            Reference: https://www.kaggle.com/datasets/teertha/personal-loan-modeling
+            """
+            df = pd.read_csv('./data/Bank_Personal_Loan_Modelling.csv')
+            df = df.sample(frac=1, random_state=1).reset_index(drop=True)
+            df = df.drop(columns=['ID'])
+                
+            self.continuous = ['CCAvg', 'Mortgage', 'Income', 'Experience', 'Age']
+            self.topology = [['Mortgage', 'Income'], ['Experience', 'Age'], ['CCAvg']]
+            self.flatten_topology =  [self.continuous.index(item) for sublist in self.topology for item in sublist]
+            df = df[self.continuous]
             
-        self.continuous = ['CCAvg', 'Mortgage', 'Income', 'Experience', 'Age']
-        self.topology = [['Mortgage', 'Income'], ['Experience', 'Age'], ['CCAvg']]
-        self.flatten_topology =  [self.continuous.index(item) for sublist in self.topology for item in sublist]
-        df = df[self.continuous]
-        
-        df_ = (df - df.mean(axis=0)) / df.std(axis=0)
-        train = df_.iloc[:4000]
-        
-        min_ = df_.min(axis=0)
-        max_ = df_.max(axis=0)
-        df = (df_ - min_) / (max_ - min_) 
-        
-        bijection = []
-        for i in range(len(self.topology)):
-            if len(self.topology[i]) == 1:
-                bijection.append(df[self.topology[i]].to_numpy())
-                continue
-            df_tmp = df[self.topology[i]].to_numpy()
-            bijection_tmp = []
-            for x, y in df_tmp:
-                bijection_tmp.append(interleave_float(x, y))
-            bijection.append(np.array([bijection_tmp]).T)
-        bijection = np.concatenate(bijection, axis=1)
-        self.label = bijection[:4000, :]
+            df_ = (df - df.mean(axis=0)) / df.std(axis=0)
+            train = df_.iloc[:4000]
             
-        self.train = train
-        self.x_data = train.to_numpy()
+            min_ = df_.min(axis=0)
+            max_ = df_.max(axis=0)
+            df = (df_ - min_) / (max_ - min_) 
+            
+            bijection = []
+            for i in range(len(self.topology)):
+                if len(self.topology[i]) == 1:
+                    bijection.append(df[self.topology[i]].to_numpy())
+                    continue
+                df_tmp = df[self.topology[i]].to_numpy()
+                bijection_tmp = []
+                for x, y in df_tmp:
+                    bijection_tmp.append(interleave_float(x, y))
+                bijection.append(np.array([bijection_tmp]).T)
+            bijection = np.concatenate(bijection, axis=1)
+            self.label = bijection[:4000, :]
+                
+            self.train = train
+            self.x_data = train.to_numpy()
+            
+        elif config["dataset"] == 'adult':
+            """
+            load dataset: Adult
+            Reference: https://archive.ics.uci.edu/ml/datasets/Adult
+            """
+            df = pd.read_csv('./data/adult.csv')
+            df = df.sample(frac=1, random_state=1).reset_index(drop=True)
+            df = df[(df == '?').sum(axis=1) == 0]
+            df['income'] = df['income'].map({'<=50K': 0, '>50K': 1, '<=50K.': 0, '>50K.': 1})
+            
+            self.continuous = ['income', 'educational-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+            self.topology = [['capital-gain'], ['capital-loss'], ['income', 'educational-num', 'hours-per-week']]
+            self.flatten_topology =  [self.continuous.index(item) for sublist in self.topology for item in sublist]
+            df = df[self.continuous]
+            
+            df_ = (df - df.mean(axis=0)) / df.std(axis=0)
+            train = df_.iloc[:40000, ]
+            
+            min_ = df_.min(axis=0)
+            max_ = df_.max(axis=0)
+            df = (df_ - min_) / (max_ - min_) 
+            
+            bijection = []
+            for i in range(len(self.topology)):
+                if len(self.topology[i]) == 1:
+                    bijection.append(df[self.topology[i]].to_numpy())
+                    continue
+                if len(self.topology[i]) == 3:
+                    df_tmp = df[self.topology[i]].to_numpy()
+                    bijection_tmp = []
+                    for x, y in df_tmp[:, :2]:
+                        bijection_tmp.append(interleave_float(x, y))
+                    tmp = np.concatenate([np.array([bijection_tmp]).T, df_tmp[:, [2]]], axis=1)
+                    bijection_tmp = []
+                    for x, y in tmp:
+                        bijection_tmp.append(interleave_float(x, y))
+                    bijection.append(np.array([bijection_tmp]).T)
+            bijection = np.concatenate(bijection, axis=1)
+            self.label = bijection[:40000, ]
+            
+            self.train = train
+            self.x_data = train.to_numpy()
+            
+        else:
+            raise ValueError('Not supported dataset!')
         
     def __len__(self): 
         return len(self.x_data)
@@ -77,41 +124,88 @@ class TabularDataset(Dataset):
 #%%
 class TestTabularDataset(Dataset): 
     def __init__(self, config):
-        """
-        load dataset: Personal Loan
-        Reference: https://www.kaggle.com/datasets/teertha/personal-loan-modeling
-        """
-        df = pd.read_csv('./data/Bank_Personal_Loan_Modelling.csv')
-        df = df.sample(frac=1, random_state=1).reset_index(drop=True)
-        df = df.drop(columns=['ID'])
+        if config["dataset"] == 'loan':
+            """
+            load dataset: Personal Loan
+            Reference: https://www.kaggle.com/datasets/teertha/personal-loan-modeling
+            """
+            df = pd.read_csv('./data/Bank_Personal_Loan_Modelling.csv')
+            df = df.sample(frac=1, random_state=1).reset_index(drop=True)
+            df = df.drop(columns=['ID'])
+                
+            self.continuous = ['CCAvg', 'Mortgage', 'Income', 'Experience', 'Age']
+            self.topology = [['Mortgage', 'Income'], ['Experience', 'Age'], ['CCAvg']]
+            self.flatten_topology =  [self.continuous.index(item) for sublist in self.topology for item in sublist]
+            df = df[self.continuous]
             
-        self.continuous = ['CCAvg', 'Mortgage', 'Income', 'Experience', 'Age']
-        self.topology = [['Mortgage', 'Income'], ['Experience', 'Age'], ['CCAvg']]
-        self.flatten_topology =  [self.continuous.index(item) for sublist in self.topology for item in sublist]
-        df = df[self.continuous]
-        
-        df_ = (df - df.mean(axis=0)) / df.std(axis=0)
-        test = df_.iloc[4000:]
-        
-        min_ = df_.min(axis=0)
-        max_ = df_.max(axis=0)
-        df = (df_ - min_) / (max_ - min_) 
-        
-        bijection = []
-        for i in range(len(self.topology)):
-            if len(self.topology[i]) == 1:
-                bijection.append(df[self.topology[i]].to_numpy())
-                continue
-            df_tmp = df[self.topology[i]].to_numpy()
-            bijection_tmp = []
-            for x, y in df_tmp:
-                bijection_tmp.append(interleave_float(x, y))
-            bijection.append(np.array([bijection_tmp]).T)
-        bijection = np.concatenate(bijection, axis=1)
-        self.label = bijection[4000:, :]
+            df_ = (df - df.mean(axis=0)) / df.std(axis=0)
+            test = df_.iloc[4000:]
             
-        self.test = test
-        self.x_data = test.to_numpy()
+            min_ = df_.min(axis=0)
+            max_ = df_.max(axis=0)
+            df = (df_ - min_) / (max_ - min_) 
+            
+            bijection = []
+            for i in range(len(self.topology)):
+                if len(self.topology[i]) == 1:
+                    bijection.append(df[self.topology[i]].to_numpy())
+                    continue
+                df_tmp = df[self.topology[i]].to_numpy()
+                bijection_tmp = []
+                for x, y in df_tmp:
+                    bijection_tmp.append(interleave_float(x, y))
+                bijection.append(np.array([bijection_tmp]).T)
+            bijection = np.concatenate(bijection, axis=1)
+            self.label = bijection[4000:, :]
+                
+            self.test = test
+            self.x_data = test.to_numpy()
+        
+        elif config["dataset"] == 'adult':
+            """
+            load dataset: Adult
+            Reference: https://archive.ics.uci.edu/ml/datasets/Adult
+            """
+            df = pd.read_csv('./data/adult.csv')
+            df = df.sample(frac=1, random_state=1).reset_index(drop=True)
+            df = df[(df == '?').sum(axis=1) == 0]
+            df['income'] = df['income'].map({'<=50K': 0, '>50K': 1, '<=50K.': 0, '>50K.': 1})
+            
+            self.continuous = ['income', 'educational-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+            self.topology = [['capital-gain'], ['capital-loss'], ['income', 'educational-num', 'hours-per-week']]
+            self.flatten_topology =  [self.continuous.index(item) for sublist in self.topology for item in sublist]
+            df = df[self.continuous]
+            
+            df_ = (df - df.mean(axis=0)) / df.std(axis=0)
+            test = df_.iloc[40000:, ]
+            
+            min_ = df_.min(axis=0)
+            max_ = df_.max(axis=0)
+            df = (df_ - min_) / (max_ - min_) 
+            
+            bijection = []
+            for i in range(len(self.topology)):
+                if len(self.topology[i]) == 1:
+                    bijection.append(df[self.topology[i]].to_numpy())
+                    continue
+                if len(self.topology[i]) == 3:
+                    df_tmp = df[self.topology[i]].to_numpy()
+                    bijection_tmp = []
+                    for x, y in df_tmp[:, :2]:
+                        bijection_tmp.append(interleave_float(x, y))
+                    tmp = np.concatenate([np.array([bijection_tmp]).T, df_tmp[:, [2]]], axis=1)
+                    bijection_tmp = []
+                    for x, y in tmp:
+                        bijection_tmp.append(interleave_float(x, y))
+                    bijection.append(np.array([bijection_tmp]).T)
+            bijection = np.concatenate(bijection, axis=1)
+            self.label = bijection[40000:, ]
+            
+            self.test = test
+            self.x_data = test.to_numpy()
+            
+        else:
+            raise ValueError('Not supported dataset!')
         
     def __len__(self): 
         return len(self.x_data)
@@ -123,43 +217,94 @@ class TestTabularDataset(Dataset):
 #%%
 class TabularDataset2(Dataset): 
     def __init__(self, config):
-        """
-        load dataset: Personal Loan
-        Reference: https://www.kaggle.com/datasets/teertha/personal-loan-modeling
-        """
-        df = pd.read_csv('./data/Bank_Personal_Loan_Modelling.csv')
-        df = df.sample(frac=1, random_state=1).reset_index(drop=True)
-        df = df.drop(columns=['ID'])
-        self.continuous = ['Mortgage', 'Income', 'Experience', 'Age', 'CCAvg']
-        df = df[self.continuous]
-        self.topology = [['Mortgage', 'Income'], ['Experience', 'Age'], ['CCAvg']]
-        
-        min_ = df.min(axis=0)
-        max_ = df.max(axis=0)
-        df_normalized = (df - min_) / (max_ - min_) 
-        
-        bijection = []
-        for i in range(len(self.topology)):
-            if len(self.topology[i]) == 1:
-                bijection.append(df_normalized[self.topology[i]].to_numpy())
-                continue
-            df_tmp = df_normalized[self.topology[i]].to_numpy()
-            bijection_tmp = []
-            for x, y in df_tmp:
-                bijection_tmp.append(interleave_float(x, y))
-            bijection.append(np.array([bijection_tmp]).T)
-        bijection = np.concatenate(bijection, axis=1)
-        self.label = bijection[:4000, :]
-        
-        df = df[self.continuous].iloc[:4000]
-        
-        transformer = DataTransformer()
-        transformer.fit(df)
-        train_data = transformer.transform(df)
-        self.transformer = transformer
+        if config["dataset"] == 'loan':
+            """
+            load dataset: Personal Loan
+            Reference: https://www.kaggle.com/datasets/teertha/personal-loan-modeling
+            """
+            df = pd.read_csv('./data/Bank_Personal_Loan_Modelling.csv')
+            df = df.sample(frac=1, random_state=1).reset_index(drop=True)
+            df = df.drop(columns=['ID'])
+            self.continuous = ['Mortgage', 'Income', 'Experience', 'Age', 'CCAvg']
+            df = df[self.continuous]
+            self.topology = [['Mortgage', 'Income'], ['Experience', 'Age'], ['CCAvg']]
             
-        self.train = train_data
-        self.x_data = torch.from_numpy(train_data.astype('float32'))
+            min_ = df.min(axis=0)
+            max_ = df.max(axis=0)
+            df_normalized = (df - min_) / (max_ - min_) 
+            
+            bijection = []
+            for i in range(len(self.topology)):
+                if len(self.topology[i]) == 1:
+                    bijection.append(df_normalized[self.topology[i]].to_numpy())
+                    continue
+                df_tmp = df_normalized[self.topology[i]].to_numpy()
+                bijection_tmp = []
+                for x, y in df_tmp:
+                    bijection_tmp.append(interleave_float(x, y))
+                bijection.append(np.array([bijection_tmp]).T)
+            bijection = np.concatenate(bijection, axis=1)
+            self.label = bijection[:4000, :]
+            
+            df = df[self.continuous].iloc[:4000]
+            
+            transformer = DataTransformer()
+            transformer.fit(df)
+            train_data = transformer.transform(df)
+            self.transformer = transformer
+                
+            self.train = train_data
+            self.x_data = torch.from_numpy(train_data.astype('float32'))
+        
+        elif config["dataset"] == 'adult':
+            """
+            load dataset: Adult
+            Reference: https://archive.ics.uci.edu/ml/datasets/Adult
+            """
+            df = pd.read_csv('./data/adult.csv')
+            df = df.sample(frac=1, random_state=1).reset_index(drop=True)
+            df = df[(df == '?').sum(axis=1) == 0]
+            df['income'] = df['income'].map({'<=50K': 0, '>50K': 1, '<=50K.': 0, '>50K.': 1})
+            
+            self.continuous = ['income', 'educational-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+            self.topology = [['capital-gain'], ['capital-loss'], ['income', 'educational-num', 'hours-per-week']]
+            self.flatten_topology =  [self.continuous.index(item) for sublist in self.topology for item in sublist]
+            df = df[self.continuous]
+            
+            min_ = df.min(axis=0)
+            max_ = df.max(axis=0)
+            df_normalized = (df - min_) / (max_ - min_) 
+            
+            bijection = []
+            for i in range(len(self.topology)):
+                if len(self.topology[i]) == 1:
+                    bijection.append(df_normalized[self.topology[i]].to_numpy())
+                    continue
+                if len(self.topology[i]) == 3:
+                    df_tmp = df_normalized[self.topology[i]].to_numpy()
+                    bijection_tmp = []
+                    for x, y in df_tmp[:, :2]:
+                        bijection_tmp.append(interleave_float(x, y))
+                    tmp = np.concatenate([np.array([bijection_tmp]).T, df_tmp[:, [2]]], axis=1)
+                    bijection_tmp = []
+                    for x, y in tmp:
+                        bijection_tmp.append(interleave_float(x, y))
+                    bijection.append(np.array([bijection_tmp]).T)
+            bijection = np.concatenate(bijection, axis=1)
+            self.label = bijection[:40000, ]
+            
+            df = df[self.continuous].iloc[:4000]
+            
+            transformer = DataTransformer()
+            transformer.fit(df)
+            train_data = transformer.transform(df)
+            self.transformer = transformer
+                
+            self.train = train_data
+            self.x_data = torch.from_numpy(train_data.astype('float32'))
+            
+        else:
+            raise ValueError('Not supported dataset!')
         
     def __len__(self): 
         return len(self.x_data)
