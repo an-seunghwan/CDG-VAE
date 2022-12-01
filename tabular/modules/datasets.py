ccd3097682ee75dbf33ae31f1ed502b34f0dc4c4
an-seunghwan <dpeltms79@gmail.com>
2022-12-01 21:09:28 +0900

    tabular: add credit dataset

--- tabular/modules/datasets.py
+++ tabular/modules/datasets.py
@@ -113,6 +113,74 @@ class TabularDataset(Dataset):
             
             self.train = train
             self.x_data = train.to_numpy()
+        
+        elif config["dataset"] == 'credit':
+            base = pd.read_csv('./data/home-credit-default-risk/application_train.csv')
+            base = base.sample(frac=1, random_state=1).reset_index(drop=True)
+            
+            self.continuous = ['TARGET', 'AMT_ANNUITY', 'AMT_CREDIT', 'DAYS_EMPLOYED', 'DAYS_BIRTH']
+            self.topology = [['TARGET'], ['AMT_ANNUITY'], ['AMT_CREDIT', 'DAYS_EMPLOYED', 'DAYS_BIRTH']]
+            df = base[self.continuous]
+            df = df.dropna(axis=0)
+            # imbalanced class
+            np.random.seed(1)
+            idx = np.random.choice(
+                range((df['TARGET'] == 0).sum()), 
+                (df['TARGET'] == 1).sum() * 1, 
+                replace=False)
+            df = pd.concat([df.iloc[idx], df[df['TARGET'] == 1]], axis=0).reset_index().drop(columns=['index'])
+            df = df.sample(frac=1, random_state=1).reset_index(drop=True)
+            
+            """remove outlier"""
+            for col in self.continuous:
+                if col == 'TARGET': continue
+                
+                q1 = df[col].quantile(0.25)
+                q3 = df[col].quantile(0.75)
+                iqr = q3 - q1
+
+                condition = df[col] > q3 + 1.5 * iqr
+                idx = df[condition].index
+                df.drop(idx, inplace=True)
+                condition = df[col] < q1 - 1.5 * iqr
+                idx = df[condition].index
+                df.drop(idx, inplace=True)
+            
+            scaling = [x for x in self.continuous if x != 'TARGET']
+            df_ = df.copy()
+            df_[scaling] = (df[scaling] - df[scaling].mean(axis=0)) / df[scaling].std(axis=0)
+            train = df_.iloc[:30000, ]
+            
+            min_ = df_.min(axis=0)
+            max_ = df_.max(axis=0)
+            df = (df_ - min_) / (max_ - min_) 
+            
+            bijection = []
+            for i in range(len(self.topology)):
+                if len(self.topology[i]) == 1:
+                    bijection.append(df[self.topology[i]].to_numpy())
+                    continue
+                if len(self.topology[i]) == 2:
+                    df_tmp = df[self.topology[i]].to_numpy()
+                    bijection_tmp = []
+                    for x, y in df_tmp:
+                        bijection_tmp.append(interleave_float(x, y))
+                    bijection.append(np.array([bijection_tmp]).T)
+                if len(self.topology[i]) == 3:
+                    df_tmp = df[self.topology[i]].to_numpy()
+                    bijection_tmp = []
+                    for x, y in df_tmp[:, :2]:
+                        bijection_tmp.append(interleave_float(x, y))
+                    tmp = np.concatenate([np.array([bijection_tmp]).T, df_tmp[:, [2]]], axis=1)
+                    bijection_tmp = []
+                    for x, y in tmp:
+                        bijection_tmp.append(interleave_float(x, y))
+                    bijection.append(np.array([bijection_tmp]).T)
+            bijection = np.concatenate(bijection, axis=1)
+            self.label = bijection[:30000, ]
+            
+            self.train = train
+            self.x_data = train.to_numpy()
             
         else:
             raise ValueError('Not supported dataset!')
@@ -209,6 +277,74 @@ class TestTabularDataset(Dataset):
             
             self.test = test
             self.x_data = test.to_numpy()
+        
+        elif config["dataset"] == 'credit':
+            base = pd.read_csv('./data/home-credit-default-risk/application_train.csv')
+            base = base.sample(frac=1, random_state=1).reset_index(drop=True)
+            
+            self.continuous = ['TARGET', 'AMT_ANNUITY', 'AMT_CREDIT', 'DAYS_EMPLOYED', 'DAYS_BIRTH']
+            self.topology = [['TARGET'], ['AMT_ANNUITY'], ['AMT_CREDIT', 'DAYS_EMPLOYED', 'DAYS_BIRTH']]
+            df = base[self.continuous]
+            df = df.dropna(axis=0)
+            # imbalanced class
+            np.random.seed(1)
+            idx = np.random.choice(
+                range((df['TARGET'] == 0).sum()), 
+                (df['TARGET'] == 1).sum() * 1, 
+                replace=False)
+            df = pd.concat([df.iloc[idx], df[df['TARGET'] == 1]], axis=0).reset_index().drop(columns=['index'])
+            df = df.sample(frac=1, random_state=1).reset_index(drop=True)
+            
+            """remove outlier"""
+            for col in self.continuous:
+                if col == 'TARGET': continue
+                
+                q1 = df[col].quantile(0.25)
+                q3 = df[col].quantile(0.75)
+                iqr = q3 - q1
+
+                condition = df[col] > q3 + 1.5 * iqr
+                idx = df[condition].index
+                df.drop(idx, inplace=True)
+                condition = df[col] < q1 - 1.5 * iqr
+                idx = df[condition].index
+                df.drop(idx, inplace=True)
+            
+            scaling = [x for x in self.continuous if x != 'TARGET']
+            df_ = df.copy()
+            df_[scaling] = (df[scaling] - df[scaling].mean(axis=0)) / df[scaling].std(axis=0)
+            test = df_.iloc[30000:, ]
+            
+            min_ = df_.min(axis=0)
+            max_ = df_.max(axis=0)
+            df = (df_ - min_) / (max_ - min_) 
+            
+            bijection = []
+            for i in range(len(self.topology)):
+                if len(self.topology[i]) == 1:
+                    bijection.append(df[self.topology[i]].to_numpy())
+                    continue
+                if len(self.topology[i]) == 2:
+                    df_tmp = df[self.topology[i]].to_numpy()
+                    bijection_tmp = []
+                    for x, y in df_tmp:
+                        bijection_tmp.append(interleave_float(x, y))
+                    bijection.append(np.array([bijection_tmp]).T)
+                if len(self.topology[i]) == 3:
+                    df_tmp = df[self.topology[i]].to_numpy()
+                    bijection_tmp = []
+                    for x, y in df_tmp[:, :2]:
+                        bijection_tmp.append(interleave_float(x, y))
+                    tmp = np.concatenate([np.array([bijection_tmp]).T, df_tmp[:, [2]]], axis=1)
+                    bijection_tmp = []
+                    for x, y in tmp:
+                        bijection_tmp.append(interleave_float(x, y))
+                    bijection.append(np.array([bijection_tmp]).T)
+            bijection = np.concatenate(bijection, axis=1)
+            self.label = bijection[30000:, ]
+            
+            self.test = test
+            self.x_data = test.to_numpy()
             
         else:
             raise ValueError('Not supported dataset!')
@@ -300,6 +436,76 @@ class TabularDataset2(Dataset):
             self.label = bijection[:40000, ]
             
             df = df[self.continuous].iloc[:4000]
+            
+            transformer = DataTransformer()
+            transformer.fit(df)
+            train_data = transformer.transform(df)
+            self.transformer = transformer
+                
+            self.train = train_data
+            self.x_data = torch.from_numpy(train_data.astype('float32'))
+        
+        elif config["dataset"] == 'credit':
+            base = pd.read_csv('./data/home-credit-default-risk/application_train.csv')
+            base = base.sample(frac=1, random_state=1).reset_index(drop=True)
+            
+            self.continuous = ['TARGET', 'AMT_ANNUITY', 'AMT_CREDIT', 'DAYS_EMPLOYED', 'DAYS_BIRTH']
+            self.topology = [['TARGET'], ['AMT_ANNUITY'], ['AMT_CREDIT', 'DAYS_EMPLOYED', 'DAYS_BIRTH']]
+            df = base[self.continuous]
+            df = df.dropna(axis=0)
+            # imbalanced class
+            np.random.seed(1)
+            idx = np.random.choice(
+                range((df['TARGET'] == 0).sum()), 
+                (df['TARGET'] == 1).sum() * 1, 
+                replace=False)
+            df = pd.concat([df.iloc[idx], df[df['TARGET'] == 1]], axis=0).reset_index().drop(columns=['index'])
+            df = df.sample(frac=1, random_state=1).reset_index(drop=True)
+            
+            """remove outlier"""
+            for col in self.continuous:
+                if col == 'TARGET': continue
+                
+                q1 = df[col].quantile(0.25)
+                q3 = df[col].quantile(0.75)
+                iqr = q3 - q1
+
+                condition = df[col] > q3 + 1.5 * iqr
+                idx = df[condition].index
+                df.drop(idx, inplace=True)
+                condition = df[col] < q1 - 1.5 * iqr
+                idx = df[condition].index
+                df.drop(idx, inplace=True)
+            
+            min_ = df.min(axis=0)
+            max_ = df.max(axis=0)
+            df_normalized = (df - min_) / (max_ - min_) 
+            
+            bijection = []
+            for i in range(len(self.topology)):
+                if len(self.topology[i]) == 1:
+                    bijection.append(df_normalized[self.topology[i]].to_numpy())
+                    continue
+                if len(self.topology[i]) == 2:
+                    df_tmp = df_normalized[self.topology[i]].to_numpy()
+                    bijection_tmp = []
+                    for x, y in df_tmp:
+                        bijection_tmp.append(interleave_float(x, y))
+                    bijection.append(np.array([bijection_tmp]).T)
+                if len(self.topology[i]) == 3:
+                    df_tmp = df_normalized[self.topology[i]].to_numpy()
+                    bijection_tmp = []
+                    for x, y in df_tmp[:, :2]:
+                        bijection_tmp.append(interleave_float(x, y))
+                    tmp = np.concatenate([np.array([bijection_tmp]).T, df_tmp[:, [2]]], axis=1)
+                    bijection_tmp = []
+                    for x, y in tmp:
+                        bijection_tmp.append(interleave_float(x, y))
+                    bijection.append(np.array([bijection_tmp]).T)
+            bijection = np.concatenate(bijection, axis=1)
+            self.label = bijection[:30000, ]
+            
+            df = df.iloc[:30000]
             
             transformer = DataTransformer()
             transformer.fit(df)
