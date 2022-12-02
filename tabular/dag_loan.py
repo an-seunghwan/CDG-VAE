2f0d7010a0fe8a99c03935f01841cbc54cd4bccc
an-seunghwan <dpeltms79@gmail.com>
2022-12-02 12:50:47 +0900

    Update dag_loan.py

--- tabular/dag_loan.py
+++ tabular/dag_loan.py
@@ -89,56 +89,6 @@ def main():
     fig = Image.open('./assets/loan/dag_bijection_loan.png')
     fig.show()
     #%%
-    # """
-    # US Census data
-    # Reference:
-    # [1] https://arxiv.org/pdf/2108.04884v3.pdf
-    # [2] https://github.com/zykls/folktables
-    # """
-    # import folktables
-    # from folktables import ACSDataSource, ACSIncome
-    # data_source = ACSDataSource(survey_year='2018', horizon='1-Year', survey='person')
-    # df = data_source.get_data(states=["CA"], download=True)
-    # ACSIncome = folktables.BasicProblem(
-    #     features=[
-    #         # 'AGEP',
-    #         'COW',
-    #         'SCHL',
-    #         'MAR',
-    #         'OCCP',
-    #         'POBP',
-    #         'RELP',
-    #         # 'WKHP',
-    #         'SEX',
-    #         'RAC1P',
-    #     ],
-    #     target='PINCP',
-    #     target_transform=lambda x: x > 50000,    
-    #     group='RAC1P',
-    #     preprocess=folktables.adult_filter,
-    #     postprocess=lambda x: np.nan_to_num(x, -1),
-    # )
-    # data, label, group = ACSIncome.df_to_numpy(df)
-    # df = pd.DataFrame(data, columns=ACSIncome.features)
-    # df = df.sample(frac=1, random_state=1).reset_index(drop=True)
-    # #%%
-    # """PC algorithm : CPDAG"""
-    # from causallearn.search.ConstraintBased.PC import pc
-    # from causallearn.utils.GraphUtils import GraphUtils
-    
-    # alpha = 0.01
-    
-    # cg = pc(data=df.to_numpy()[:5000, :], 
-    #         alpha=alpha, 
-    #         indep_test='chisq') 
-    # print(cg.G)
-    # #%%
-    # # visualization
-    # pdy = GraphUtils.to_pydot(cg.G, labels=df.columns)
-    # pdy.write_png('./assets/dag_acsincome.png')
-    # fig = Image.open('./assets/dag_acsincome.png')
-    # fig.show()
-    #%%
 #%%
 if __name__ == '__main__':
     main()
